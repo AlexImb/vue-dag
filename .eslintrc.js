b6cb408d69ca3c095ba823c8d70b5a29bace687a
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2019-09-23 22:25:57 +0200

    :rotating_light: Use more strict lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        singleQuote: true,
+        trailingComma: 'all',
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: '@typescript-eslint/parser',
   },
   overrides: [
     {
-      files: ["**/__tests__/*.{j,t}s?(x)"],
+      files: ['**/__tests__/*.{j,t}s?(x)'],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
